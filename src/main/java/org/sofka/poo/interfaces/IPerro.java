c9ffeb4f8ff8c73fa26d8bfb7860a5af0edf0188
Oscar Farfan <oscarfarfan92@gmail.com>
2022-05-27 22:01:27 -0500

    Se realizan algunas observaciones a los comentarios de la Interfaces Perro y Gato

--- src/main/java/org/sofka/poo/interfaces/IPerro.java
+++ src/main/java/org/sofka/poo/interfaces/IPerro.java
@@ -1,5 +1,12 @@
 package org.sofka.poo.interfaces;
 
+/**
+ * La Interface Perro define los métodos que pueden ser implementados en otras clases
+ *
+ * @author Óscar Farfán - oscarfarfan92@gmail.com
+ *
+ * @version 1.0.0.000 27-05-2022
+ */
 public interface IPerro {
 
     public void ladrar();
